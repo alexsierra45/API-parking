2147a60bbe395bffa20b6e8dffaed36a65750a3e
alexsierra45 <alexsierraalcala@gmail.com>
2023-07-28 12:04:46 -0400

    apps routes

--- src/network/routes.js
+++ src/network/routes.js
@@ -2,11 +2,14 @@ const express = require('express');
 const parking = require('../components/parking/network');
 const user = require('../components/user/network');
 const auth = require('../components/auth/network');
+const { authenticateToken,  authorizeRole } = require('../components/auth/middleware');
 
 const routes = function (server) {
     server.use('/login', auth);
+    
+    server.use(authenticateToken);
     server.use('/parking', parking);
-    server.use('/user', user);
+    server.use('/user',  authorizeRole('admin'), user);
 }
 
 module.exports = routes;
